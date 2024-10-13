28a9dbb0c5fb5ff3f54a66ccac2b9bb10c347447
Mark Giannelli <giannellimj@gmail.com>
2024-10-13 18:02:01 -0500

    add loading to contact page and reset input fields after successful send

--- src/app-2d/views/contact/contact.view-controller.tsx
+++ src/app-2d/views/contact/contact.view-controller.tsx
@@ -1,6 +1,6 @@
 import { send } from 'emailjs-com';
 import { useState, ChangeEvent, FormEvent } from 'react';
-import { validateEmail } from '../../../utilties/utils';
+import { capitalizeFirstLetter, validateEmail } from '../../../utilties/utils';
 
 // Define the types for form state and response
 interface FormState {
@@ -10,6 +10,7 @@ interface FormState {
 }
 
 const ContactViewController = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [formState, setFormState] = useState<FormState>({
     name: '',
     email: '',
@@ -22,6 +23,7 @@ const ContactViewController = () => {
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
+    setErrorMessage('');
     const { name, value } = e.target;
 
     if (name === 'email') {
@@ -33,7 +35,7 @@ const ContactViewController = () => {
       }
     } else {
       if (!value.length) {
-        setErrorMessage(`${name} is required.`);
+        setErrorMessage(`${capitalizeFirstLetter(name)} is required.`);
       } else {
         setErrorMessage('');
       }
@@ -46,22 +48,12 @@ const ContactViewController = () => {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    setLoading(true);
     const from_name = formState.name;
     const message = formState.message;
     const reply_to = formState.email;
 
     const toSend = { from_name, message, reply_to };
-
-    // Reset input fields after submit
-    Array.from(document.querySelectorAll<HTMLInputElement>('input')).forEach(
-      (input) => (input.value = ''),
-    );
-
-    // Reset textarea fields after submit
-    Array.from(
-      document.querySelectorAll<HTMLTextAreaElement>('textarea'),
-    ).forEach((input) => (input.value = ''));
 
     send(
       'service_106gwhj',
@@ -70,13 +62,22 @@ const ContactViewController = () => {
       'user_NVXWfdcOsviqZFCpy1AWZ',
     )
       .then((response) => {
+        setFormState({
+          name: '',
+          email: '',
+          message: '',
+        });
         setResponse(response);
+        setLoading(false);
         setTimeout(() => {
           setResponse(null);
         }, 2000);
       })
       .catch((err) => {
         console.log('FAILED...', err);
+        setErrorMessage(
+          'There was error trying to send your email. Please try again.',
+        );
       });
   };
   return {
@@ -87,6 +88,7 @@ const ContactViewController = () => {
     message,
     errorMessage,
     response,
+    loading,
   };
 };
 
--- src/app-2d/views/contact/contact.view.tsx
+++ src/app-2d/views/contact/contact.view.tsx
@@ -1,3 +1,5 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ContactViewController from './contact.view-controller';
 import './contact.view.scss';
 
@@ -10,62 +12,71 @@ const Contact: React.FC = () => {
     message,
     errorMessage,
     response,
+    loading,
   } = ContactViewController();
   return (
-    <section>
-      <h1 className="title">
-        Contact me below or @{' '}
-        <a id="email-link" href="mailto: giannellimj@gmail.com">
-          giannellimj@gmail.com
-        </a>
-      </h1>
-      <div id="contact-form-div" className="box">
-        <form id="contact-form" onSubmit={handleSubmit}>
-          <div className="row form">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              name="name"
-              defaultValue={name}
-              onBlur={handleChange}
-            />
+    <>
+      {loading ? (
+        <div className="loading">
+          <FontAwesomeIcon icon={faSpinner} />
+        </div>
+      ) : (
+        <section>
+          <h1 className="title">
+            Contact me below or @{' '}
+            <a id="email-link" href="mailto: giannellimj@gmail.com">
+              giannellimj@gmail.com
+            </a>
+          </h1>
+          <div id="contact-form-div" className="box">
+            <form id="contact-form" onSubmit={handleSubmit}>
+              <div className="row form">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  defaultValue={name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="row form">
+                <label htmlFor="email">Email address:</label>
+                <input
+                  type="email"
+                  name="email"
+                  defaultValue={email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="row form">
+                <label htmlFor="message">Message:</label>
+                <textarea
+                  name="message"
+                  rows={5}
+                  defaultValue={message}
+                  onChange={handleChange}
+                />
+              </div>
+              {errorMessage && (
+                <div>
+                  <p className="error-text">{errorMessage}</p>
+                </div>
+              )}
+              <div id="form-btn-div" className="row">
+                <button id="form-btn" type="submit">
+                  Submit
+                </button>
+              </div>
+              {response && (
+                <div>
+                  <p className="response">Message Successfully Sent!</p>
+                </div>
+              )}
+            </form>
           </div>
-          <div className="row form">
-            <label htmlFor="email">Email address:</label>
-            <input
-              type="email"
-              name="email"
-              defaultValue={email}
-              onBlur={handleChange}
-            />
-          </div>
-          <div className="row form">
-            <label htmlFor="message">Message:</label>
-            <textarea
-              name="message"
-              rows={5}
-              defaultValue={message}
-              onBlur={handleChange}
-            />
-          </div>
-          {errorMessage && (
-            <div>
-              <p className="error-text">{errorMessage}</p>
-            </div>
-          )}
-          <div id="form-btn-div" className="row">
-            <button id="form-btn" type="submit">
-              Submit
-            </button>
-          </div>
-          {response && (
-            <div>
-              <p className="response">Message Successfully Sent!</p>
-            </div>
-          )}
-        </form>
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 };
 
